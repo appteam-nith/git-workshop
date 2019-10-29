9cc444c063d25bd596bf43889ff80bb184c7b3c3
hardik123321 <jc664305@gmail.com>
2019-10-29 13:57:09 +0530

    second commit

---
+++ submissions/2018/hardik/c.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define fast()                        \
+ios_base::sync_with_stdio(false); \
+cin.tie(NULL);
+#define endl "\n"
+
+int main(){
+	
+	fast()
+	
+	#ifndef ONLINE_JUDGE
+	freopen("./input.txt", "r", stdin);
+	freopen("./output.txt", "w", stdout);
+	#endif
+	string s;
+	cin>>s;
+	cout<<s<<endl;
+	return 0;
+}
